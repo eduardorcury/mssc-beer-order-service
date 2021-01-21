196074ad5e286e6cb7ffa2f01b325db36df74cd2
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2021-01-21 18:59:58 -0300

    Adding State Machine transitions

--- src/main/java/guru/sfg/beer/order/service/domain/Customer.java
+++ src/main/java/guru/sfg/beer/order/service/domain/Customer.java
@@ -20,6 +20,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
